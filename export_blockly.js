e73388a9be1fea33f2a2209d0ab6772d32e82b02
Loren Norman <lorennorman@gmail.com>
2024-03-05 12:28:43 -0500

    tidy export a bit, quick/dirty js template replacement

--- export_blockly.js
+++ export_blockly.js
@@ -1,30 +1,40 @@
 import fs from 'fs'
-import { map } from 'lodash-es'
 
-import { customBlocksJson } from './src/blocks/index.js'
-import allGenerators from './src/blocks/generators.js'
-import toolbox from './src/toolboxes/index.js'
 
+// CLEAN
 // export/
 if(fs.existsSync('export')) { fs.rmSync('export', { recursive: true, force: true }) }
 fs.mkdirSync('export')
 
+// JSON FILES
 // export/workspace.json
 fs.copyFileSync('./src/workspaces/workspace.json', 'export/workspace.json')
 
 // export/blocks.json
+import { customBlocksJson } from './src/blocks/index.js'
 fs.writeFileSync('export/blocks.json', JSON.stringify(customBlocksJson, null, 2))
 
 // export/toolbox.json
+import toolbox from './src/toolboxes/index.js'
 fs.writeFileSync('export/toolbox.json', JSON.stringify(toolbox, null, 2))
 
+// JS FILES
+// quick/dirty js template/inline replacement
+
 // export/generators.js
-// writing an executable file is nasty business
-//
-// read in the file
+import { renderedBlockGenerators } from './src/blocks/generators.js'
+// read
 const generatorsFile = fs.readFileSync('src/blocks/generators.js').toString()
-// replace the import with an inlined collection of block definitions
-const generators = 'const allBlockDefinitions = {\n' + map(allGenerators.json.forBlock, (func, block) => `  ${block}: { generators: { json: ${func} } }`).join(',\n\n') + '\n}\n'
-const generatorOutput = generatorsFile.replace("import { allBlockDefinitions } from './index.js'", generators).replaceAll('\n    ', '\n  ')
-// write the file back
+// transform
+const generatorOutput = generatorsFile.replace(/\/\* LOCAL->>[\s\S]*?<<-LOCAL \*\//, renderedBlockGenerators)
+// write
 fs.writeFileSync('export/generators.js', generatorOutput)
+
+// export/extensions.js
+import { renderedExtensions } from './src/extensions/index.js'
+// read
+const extensionsFile = fs.readFileSync('src/extensions/index.js').toString()
+// transform
+const extensionOutput = extensionsFile.replace(/\/\* LOCAL->>[\s\S]*?<<-LOCAL \*\//, renderedExtensions)
+// write
+fs.writeFileSync('export/extensions.js', extensionOutput)
