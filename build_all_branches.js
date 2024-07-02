496e70a41eeaf61cb1d09c0d54c4a5dd9c562bd0
Loren Norman <lorennorman@gmail.com>
2024-07-02 17:07:38 -0400

    support GH:P base paths

--- build_all_branches.js
+++ build_all_branches.js
@@ -1,5 +1,7 @@
 import { promisify } from 'util'
 import { exec } from 'child_process'
+import { argv } from 'process'
+
 
 const pexec = promisify(exec)
 
@@ -11,15 +13,20 @@ const build_all_branches = async () => {
     console.error("Oops, not on main branch. Switch to main and try again?")
   }
 
+  let prependBase = "/"
+  if(argv[2]?.startsWith("--base=")) {
+    prependBase = argv[2].split("=")[1]
+  }
+
+  console.log("Prepending base:", prependBase)
   // get all non-main branches
   const branches = stdout
     .split("\n")
     .filter(branch => branch.length && branch != "* main")
     .map(branch => branch.trim())
-  console.log("Branches:\n", branches)
 
   // build main
-  await pexec("vite build")
+  await pexec(`vite build --base=${prependBase}`)
 
   try {
     // for each other branch:
@@ -29,7 +36,7 @@ const build_all_branches = async () => {
       //   check out branch
       await pexec(`git checkout ${branch}`)
       //   build with outDir=dist/{branch_name} and no clean
-      await pexec(`vite build --outDir dist/${branch} --base /${branch}/`)
+      await pexec(`vite build --outDir dist/${branch} --base ${prependBase}${branch}/`)
     }
 
   } finally {
