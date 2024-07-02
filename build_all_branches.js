3714a63f0217df0cb355adc921b61184af2b0ea4
Loren Norman <lorennorman@gmail.com>
2024-07-02 17:14:43 -0400

    logging

--- build_all_branches.js
+++ build_all_branches.js
@@ -17,22 +17,23 @@ const build_all_branches = async () => {
   if(argv[2]?.startsWith("--base=")) {
     prependBase = argv[2].split("=")[1]
   }
-
   console.log("Prepending base:", prependBase)
+
   // get all non-main branches
   const branches = stdout
     .split("\n")
     .filter(branch => branch.length && branch != "* main")
     .map(branch => branch.trim())
 
-  // build main
+  console.log("Building main...")
   await pexec(`vite build --base=${prependBase}`)
 
+  console.log("Branches to build:", branches)
   try {
     // for each other branch:
     for(let i = 0; i < branches.length; i++) {
       const branch = branches[i]
-      console.log(`Building branch: ${branch}`)
+      console.log(`Building branch: ${branch}...`)
       //   check out branch
       await pexec(`git checkout ${branch}`)
       //   build with outDir=dist/{branch_name} and no clean
