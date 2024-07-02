0a985dfe673a481f9bd72e57b1922d2e657f793a
Loren Norman <lorennorman@gmail.com>
2024-07-02 16:33:54 -0400

    build all WIP

---
+++ build_all_branches.js
@@ -0,0 +1,36 @@
+import { promisify } from 'util'
+import { exec } from 'child_process'
+
+const pexec = promisify(exec)
+
+const build_all_branches = async () => {
+  // check we're on main
+  const { stdout } = await pexec("git branch")
+
+  if(!stdout.match(/^\* main$/m)) {
+    console.error("Oops, not on main branch. Switch to main and try again?")
+  }
+
+  // get all non-main branches
+  const branches = stdout
+    .split("\n")
+    .filter(branch => branch.length && branch != "* main")
+    .map(branch => branch.trim())
+  console.log("Branches:\n", branches)
+
+  // build main
+  await pexec("vite build")
+
+  // for each other branch:
+  for(let i = 0; i < branches.length; i++) {
+    const branch = branches[i]
+    //   check out branch
+    await pexec(`git checkout ${branch}`)
+    //   build with outDir=dist/{branch_name}
+    await pexec("vite build --outDir=dist/${branch}")
+  }
+
+  await pexec(`git checkout main`)
+}
+
+build_all_branches()
