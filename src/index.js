85b1a5f567f64018f9331c0c9e5ba0ebba876273
Loren Norman <lorennorman@gmail.com>
2024-07-03 10:00:06 -0400

    auto-fix when browser cache is stale

--- src/index.js
+++ src/index.js
@@ -40,41 +40,43 @@ Blockly.VerticalFlyout.prototype.getFlyoutScale = () => 1
 // inject workspace blocks
 Blockly.serialization.workspaces.load(initialWorkspace, workspace)
 
+const workspaceToBytecode = workspace => {
+  return allGenerators.json.workspaceToCode(workspace) || ""
+}
+
 // prepare generators and their dom targets
 const blocklyJsonOutputDiv = document.getElementById('blockly-json')
 const bytecodeJsonOutputDiv = document.getElementById('bytecode-json')
 const regenerate = () => {
-  const json = allGenerators.json.workspaceToCode(workspace)
+  bytecodeJsonOutputDiv.innerText = ``
+  blocklyJsonOutputDiv.innerText = ``
 
-  if(!json) {
-    bytecodeJsonOutputDiv.innerText = "workspaceToCode(workspace) created nothing"
-    blocklyJsonOutputDiv.innerText = "workspaceToCode(workspace) created nothing"
-    return
-  }
+  const
+    bytecodeJson = workspaceToBytecode(workspace),
+    bytecode = JSON.parse(bytecodeJson)
+  bytecodeJsonOutputDiv.innerText = `Bytecode is valid JSON ✅\n\n${bytecodeJson}`
 
-  try {
-    let valid = true
-    try { JSON.parse(json) }
-    catch(e) {
-      valid = false
-      console.error('Failed to JSON.parse:', json)
-      console.error(e)
-    }
-    const validation = `Bytecode JSON is ${valid ? 'valid ✅' : 'invalid ❌'}`
-    bytecodeJsonOutputDiv.innerText = `${validation}\n\n${json}`
-    // if(valid) { return }
-    // console.log(validation)
-    // console.log(json)
-  } catch(e) {
-    blocklyJsonOutputDiv.innerText = `JSON Generation Failed for:\n${json}\n\n Failed with ${e}`
-    console.error(e)
-    return
-  }
-
-  const bytecode = allRegenerators.json.codeToWorkspace(JSON.parse(json))
-  blocklyJsonOutputDiv.innerText = JSON.stringify(bytecode, null, 2)
+  const
+    workspaceObject = allRegenerators.json.codeToWorkspace(bytecode),
+    workspaceJson = JSON.stringify(workspaceObject, null, 2)
+  blocklyJsonOutputDiv.innerText = `Workspace JSON\n\n${workspaceJson}`
 }
 
+const safeRegenerate = () => {
+  try{
+    regenerate()
+  } catch(error) {
+    console.error(error)
+    console.log("safeRegenerate() caught the above")
+
+    if(!bytecodeJsonOutputDiv.innerText) {
+      bytecodeJsonOutputDiv.innerText = `Bytecode generation failed ❌\nYou may need the "Clear" button above.`
+      blocklyJsonOutputDiv.innerText = `Workspace JSON not generated.`
+    } else {
+      blocklyJsonOutputDiv.innerText = `Workspace JSON generation failed ❌\nYou may need the "Clear" button above.`
+    }
+  }
+}
 // register listeners
 
 // enforce one top-level block
@@ -91,7 +93,7 @@ workspace.addChangeListener((e) => {
   { return }
 
   // generate next cycle so orphans get disabled first
-  setTimeout(regenerate)
+  setTimeout(safeRegenerate)
 })
 
 // provide a way to clear the workspace and persistent memory
@@ -106,7 +108,7 @@ clearButton.addEventListener('click', clearAndInitialize)
 const reloadBytecodeButton = document.getElementById('button-reload-bytecode')
 reloadBytecodeButton.addEventListener('click', () => {
   // export bytecode
-  const bytecodeJson = allGenerators.json.workspaceToCode(workspace)
+  const bytecodeJson = workspaceToBytecode(workspace)
   // convert bytecode to workspace json
   const workspaceJson = allRegenerators.json.codeToWorkspace(JSON.parse(bytecodeJson))
 
@@ -136,9 +138,21 @@ reloadSerializedButton.addEventListener('click', () => {
   load(workspace)
 })
 
-// load last sketch from storage
-if(!load(workspace)) {
+// INITIALIZE ON LOAD
+
+// try to refresh and regenerate last workspace
+try {
+  // load last sketch from storage
+  if(!load(workspace)) {
+    throw "Load from cache failed."
+  }
+  // run the generators
+  regenerate()
+
+} catch(e) {
+  console.error(e)
+  console.log("Refresh and regenerate from browser cache failed with the above error, clearing cache and reinitializing...")
   clearAndInitialize()
+  regenerate()
+  console.log("Done.")
 }
-// run the generators
-regenerate()
