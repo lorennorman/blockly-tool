11613007af55b2f0cf14e8ffa8b0e05c8e9ea457
Loren Norman <lorennorman@gmail.com>
2024-07-10 14:16:33 -0400

    workspace hot reload

--- src/importer.js
+++ src/importer.js
@@ -2,7 +2,7 @@ import { find, keys, map } from 'lodash-es'
 
 import { importBlockJson } from './importer/block_importer.js'
 import importToolboxJson from './importer/toolbox_importer.js'
-import workspaceJson from './importer/workspace_importer.js'
+import importWorkspaceJson from './importer/workspace_importer.js'
 
 import extensions_js from './importer/extension_importer.js'
 import mutators_js from './importer/mutator_importer.js'
@@ -13,7 +13,7 @@ import regenerators_js from './importer/regenerator_importer.js'
 const PROCESSORS = {
   "/blocks.json": async () => JSON.stringify(await importBlockJson(), null, 2),
   "/toolbox.json": async () => JSON.stringify(await importToolboxJson(), null, 2),
-  "/workspace.json": () => JSON.stringify(workspaceJson, null, 2),
+  "/workspace.json": async () => JSON.stringify(await importWorkspaceJson(), null, 2),
   "/extensions.js": extensions_js,
   "/mutators.js": mutators_js,
   "/generators.js": generators_js,
