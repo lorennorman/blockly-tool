41e20d1aaaf64648eb248398ba903b2a770b1348
Loren Norman <lorennorman@gmail.com>
2024-11-19 13:39:04 -0500

    decompose/compose routines

--- app/blocks/root/root_options_mutator.js
+++ app/blocks/root/root_options_mutator.js
@@ -7,52 +7,56 @@ export default {
   delayMode: 'extend',
 
   saveExtraState: function() {
-    // console.log("saving root delay settings:", {
-    //   delay: this.delaySeconds,
-    //   delayMode: this.delayMode
-    // })
-
     return {
       delaySeconds: this.delaySeconds,
       delayMode: this.delayMode,
     }
   },
 
-  loadExtraState: function(state) {
-    this.delaySeconds = state.delaySeconds || 0
-    this.delayMode = state.delayMode || "extend"
-    // console.log("loading root delay settings:", {
-    //   delay: this.delaySeconds,
-    //   delayMode: this.delayMode
-    // })
+  loadExtraState: function({ delaySeconds, delayMode }) {
+    this.delaySeconds = delaySeconds || 0
+    this.delayMode = delayMode || "extend"
   },
 
   decompose: function(workspace) {
-    const delayBlock = workspace.newBlock('delay_settings')
-    delayBlock.initSvg()
+    const delaySettingsBlock = workspace.newBlock('delay_settings')
+    delaySettingsBlock.initSvg()
 
-    console.log("decomposing:", this.delaySeconds)
-    if(this.delaySeconds < 60) {
-      const delayPeriodBlock = workspace.newBlock("delay_seconds")
-      delayPeriodBlock.initSvg()
+    const delayBlockType = (this.delaySeconds < 60)
+      ? "delay_seconds"
+      : (this.delaySeconds < 3600)
+      ? "delay_minutes"
+      : (this.delaySeconds < 86400)
+      ? "delay_hours"
+      : (this.delaySeconds == 86400)
+      ? "delay_days"
+      : "delay_seconds"
+
+    const delayPeriodBlock = workspace.newBlock(delayBlockType)
+    delayPeriodBlock.initSvg()
+
+    if(delayBlockType !== "delay_days") {
+      // set its seconds field (day doesn't have one)
       delayPeriodBlock.setFieldValue(this.delaySeconds.toString(), "SECONDS")
-
-      const
-        { connection } = delayBlock.getInput("DELAY_PERIOD"),
-        { outputConnection } = delayPeriodBlock
-
-      connection.connect(outputConnection)
     }
 
-    return delayBlock
+    // connect it to the delay settings block
+    const
+      { connection } = delaySettingsBlock.getInput("DELAY_PERIOD"),
+      { outputConnection } = delayPeriodBlock
+
+    connection.connect(outputConnection)
+
+    return delaySettingsBlock
   },
 
   compose: function(delayBlock) {
     const
       periodInput = delayBlock.getInputTargetBlock("DELAY_PERIOD"),
-      value = periodInput?.getFieldValue("SECONDS") || 0
+      value = (periodInput?.type == "delay_days") // hard-code for day
+        ? "86400"
+        : periodInput?.getFieldValue("SECONDS") || 0
 
-    console.log('compose', periodInput, value)
     this.delaySeconds = parseInt(value, 10)
   }
 }
