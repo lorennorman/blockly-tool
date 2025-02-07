c94dc1753932d65931b4c9bce4b0b5d942569905
Loren Norman <lorennorman@gmail.com>
2025-02-07 14:25:34 -0500

    add all available weather metrics by time

--- app/blocks/power_ups/weather_mixin.js
+++ app/blocks/power_ups/weather_mixin.js
@@ -1,13 +1,146 @@
 // helper mixin for the weather block
 // simplifies juggling the weather api keys by period type
 export default {
-  propertyOptionsForTime: function(timeKey) {
-    return this.propertyOptions
+  // helper to humanize camelCase strings
+  keyToLabel: key => {
+    const label = key
+      // insert spaces before each capital letter
+      .replaceAll(/[A-Z]/g, (match) => `\u00A0${match}`)
+      // upcase the first letter
+      .replace(/^[a-z]/, (match) => match.toUpperCase())
+
+    return label
   },
 
-  propertyOptions: [
-    [`Option A idx}`, "option_a"],
-    [`Option B idx+}`, "option_b"],
-    [`idx+} C Option `, "option_c"]
+  propertyOptionsForTime: function(timeKey) {
+    let optionKeys
+    if(timeKey === 'current') {
+      optionKeys = this.CURRENT_PROPS
+
+    } else if(timeKey.match(/minutes/)) {
+      optionKeys = this.MINUTE_PROPS
+
+    } else if(timeKey.match(/hours/)) {
+      optionKeys = this.HOUR_PROPS
+
+    } else if(timeKey.match(/days/)) {
+      optionKeys = this.DAY_PROPS.concat(
+        this.DAYTIME_OVERNIGHT_PROPS.flatMap(prop =>
+          [`daytime:${prop}`, `overnight:${prop}` ]
+        )
+      )
+
+    } else {
+      // throw new Error(`[mixins.weather] timeKey not recognized: ${timeKey}`)
+      return [["oops", ""]]
+    }
+
+    const propertyOptions = optionKeys.reduce((acc, key) => {
+      const label = this.keyToLabel(key)
+      acc.push([ label, key ])
+
+      return acc
+    }, [])
+
+    return propertyOptions
+  },
+
+  CURRENT_PROPS: [
+    // 'asOf',
+    'cloudCover',
+    'cloudCoverLowAltPct',
+    'cloudCoverMidAltPct',
+    'cloudCoverHighAltPct',
+    'conditionCode',
+    'daylight',
+    'humidity',
+    'precipitationIntensity',
+    'pressure',
+    'pressureTrend',
+    'temperature',
+    'temperatureApparent',
+    'temperatureDewPoint',
+    'uvIndex',
+    'visibility',
+    'windDirection',
+    'windGust',
+    'windSpeed'
   ],
+
+  MINUTE_PROPS: [
+    // 'startTime',
+    'precipitationChance',
+    'precipitationIntensity'
+  ],
+
+  HOUR_PROPS: [
+    // 'forecastStart',
+    'cloudCover',
+    'conditionCode',
+    'daylight',
+    'humidity',
+    'precipitationAmount',
+    'precipitationIntensity',
+    'precipitationChance',
+    'precipitationType',
+    'pressure',
+    'pressureTrend',
+    'snowfallIntensity',
+    'snowfallAmount',
+    'temperature',
+    'temperatureApparent',
+    'temperatureDewPoint',
+    'uvIndex',
+    'visibility',
+    'windDirection',
+    'windGust',
+    'windSpeed'
+  ],
+
+  DAY_PROPS: [
+    // 'forecastStart',
+    // 'forecastEnd',
+    'conditionCode',
+    'maxUvIndex',
+    'moonPhase',
+    'moonrise',
+    'moonset',
+    'precipitationAmount',
+    'precipitationChance',
+    'precipitationType',
+    'snowfallAmount',
+    'solarMidnight',
+    'solarNoon',
+    'sunrise',
+    'sunriseCivil',
+    'sunriseNautical',
+    'sunriseAstronomical',
+    'sunset',
+    'sunsetCivil',
+    'sunsetNautical',
+    'sunsetAstronomical',
+    'temperatureMax',
+    'temperatureMin',
+    'windGustSpeedMax',
+    'windSpeedAvg',
+    'windSpeedMax',
+  ],
+
+  DAYTIME_OVERNIGHT_PROPS: [
+    // 'forecastStart',
+    // 'forecastEnd',
+    'cloudCover',
+    'conditionCode',
+    'humidity',
+    'precipitationAmount',
+    'precipitationChance',
+    'precipitationType',
+    'snowfallAmount',
+    'temperatureMax',
+    'temperatureMin',
+    'windDirection',
+    'windGustSpeedMax',
+    'windSpeed',
+    'windSpeedMax'
+  ]
 }
