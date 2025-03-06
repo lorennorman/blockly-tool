5ffca726171a1ff2cb7749a22a02f5d82468b30d
Loren Norman <lorennorman@gmail.com>
2025-03-06 15:16:56 -0500

    drop day 10, weatherkit only goes to 9. tidy.

--- app/blocks/power_ups/weather.js
+++ app/blocks/power_ups/weather.js
@@ -50,40 +50,39 @@ export default {
     [ "At:", {
       field: "POWER_UP_ID",
       options: [
-        [ "Loading locations...", ""],
+        [ "Loading locations...", "" ],
       ]
     }],
 
     [ "When:", {
       field: "WEATHER_TIME",
       options: [
-        [ "Now", "current"],
-        [ "In 5 minutes", "forecast_minutes_5"],
-        [ "In 30 minutes", "forecast_minutes_30"],
-        [ "This hour", "forecast_hours_0"],
-        [ "In 1 hour", "forecast_hours_1"],
-        [ "In 2 hours", "forecast_hours_2"],
-        [ "In 6 hours", "forecast_hours_6"],
-        [ "In 12 hours", "forecast_hours_12"],
-        [ "In 24 hours", "forecast_hours_24"],
-        [ "Today", "forecast_days_0"],
-        [ "Tomorrow", "forecast_days_1"],
-        [ "In 2 days", "forecast_days_2"],
-        [ "In 3 days", "forecast_days_3"],
-        [ "In 4 days", "forecast_days_4"],
-        [ "In 5 days", "forecast_days_5"],
-        [ "In 6 days", "forecast_days_6"],
-        [ "In 7 days", "forecast_days_7"],
-        [ "In 8 days", "forecast_days_8"],
-        [ "In 9 days", "forecast_days_9"],
-        [ "In 10  days", "forecast_days_10"],
+        [ "Now", "current" ],
+        [ "In 5 minutes", "forecast_minutes_5" ],
+        [ "In 30 minutes", "forecast_minutes_30" ],
+        [ "This hour", "forecast_hours_0" ],
+        [ "In 1 hour", "forecast_hours_1" ],
+        [ "In 2 hours", "forecast_hours_2" ],
+        [ "In 6 hours", "forecast_hours_6" ],
+        [ "In 12 hours", "forecast_hours_12" ],
+        [ "In 24 hours", "forecast_hours_24" ],
+        [ "Today", "forecast_days_0" ],
+        [ "Tomorrow", "forecast_days_1" ],
+        [ "In 2 days", "forecast_days_2" ],
+        [ "In 3 days", "forecast_days_3" ],
+        [ "In 4 days", "forecast_days_4" ],
+        [ "In 5 days", "forecast_days_5" ],
+        [ "In 6 days", "forecast_days_6" ],
+        [ "In 7 days", "forecast_days_7" ],
+        [ "In 8 days", "forecast_days_8" ],
+        [ "In 9 days", "forecast_days_9" ],
       ]
     }],
 
     [ "Metric:", {
       field: "WEATHER_PROPERTY",
       options: [
-        [ "select", "cloudCover"], // default to a real value to avoid a warning on block creation
+        [ "select", "cloudCover" ], // default to a real value to avoid a warning on block creation
       ]
     }],
   ],
