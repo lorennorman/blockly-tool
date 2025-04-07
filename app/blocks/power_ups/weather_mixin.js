6cf94c77a6840aaceb7b0d0f112be8569ea7ca04
Loren Norman <lorennorman@gmail.com>
2025-04-07 17:01:36 -0400

    rename "values" to "example"

--- app/blocks/power_ups/weather_mixin.js
+++ app/blocks/power_ups/weather_mixin.js
@@ -29,7 +29,10 @@ export default {
   keyToCurrent: function(key) {
     const { values="unknown" } = this.keyToHelpObject(key)
 
-    return values
+    // use example value otherwise
+    const { example="unknown" } = this.keyToHelpObject(key)
+
+    return example
   },
 
   propertyOptionsForTime: function(timeKey) {
@@ -191,171 +194,171 @@ export default {
 
   HELP_TEXT_BY_PROP: {
     cloudCover: {
-      values: "0.0",
+      example: "0.0",
       description: "The percentage of the sky covered with clouds during the period."
     },
     cloudCoverLowAltPct: {
-      values: "0.0",
+      example: "0.0",
       description: "The percentage of the sky covered with low-altitude clouds (1800m and below) during the period."
     },
     cloudCoverMidAltPct: {
-      values: "0.0",
+      example: "0.0",
       description: "The percentage of the sky covered with mid-altitude clouds (1800m to 6300m) during the period."
     },
     cloudCoverHighAltPct: {
-      values: "0.0",
+      example: "0.0",
       description: "The percentage of the sky covered with high-altitude clouds (6300m and above) during the period."
     },
     conditionCode: {
-      values: '"Clear"',
+      example: '"Clear"',
       description: "Weather conditions at the given time, one of: BlowingDust, Clear, MostlyClear, PartlyCloudy, MostlyCloudy, Cloudy, Foggy, Haze, Smoky, Breezy, Windy, Drizzle, Rain, HeavyRain, IsolatedThunderstorms, ScatteredThunderstorms, Thunderstorms, StrongStorms, SunShowers, Frigid, Hail, Hot, Flurries, Sleet, Snow, SunFlurries, WintryMix, Blizzard, BlowingSnow, FreezingDrizzle, FreezingRain, HeavySnow, Hurricane, TroicalStorm"
     },
     daylight: {
-      values: "true",
+      example: "true",
       description: "Is there daylight at this time?"
     },
     humidity: {
-      values: "0.0",
+      example: "0.0",
       description: "Relative humidity as a percentage of the total water vapor the air can hold at this temperature."
     },
     precipitationIntensity: {
-      values: "13.2",
+      example: "13.2",
       description: "Precipitation intensity measured in millimeters per hour"
     },
     pressure: {
-      values: "1,013.25",
+      example: "1,013.25",
       description: "The sea level air pressure, in millibars."
     },
     pressureTrend: {
-      values: '"rising"',
+      example: '"rising"',
       description: "A string indicating the sea level air pressure trend during this period."
     },
     temperature: {
-      values: "22.7",
+      example: "22.7",
       description: "Temperature in degrees Celsius during this period."
     },
     temperatureApparent: {
-      values: "38.5...",
+      example: "38.5...",
       description: "The feels-like temperature when factoring wind and humidity, in degrees Celsius during this period."
     },
     temperatureDewPoint: {
-      values: "26.3",
+      example: "26.3",
       description: "The temperature at which relative humidity is 100%, in degrees Celsius during this period."
     },
     uvIndex: {
-      values: "5",
+      example: "5",
       description: "The level of ultraviolet radiation during this period."
     },
     visibility: {
-      values: "28520.77",
+      example: "28520.77",
       description: "The distance at which terrain is visible, in meters. Under 1,000 is considered \"low visibilty\""
     },
     windDirection: {
-      values: "0",
+      example: "0",
       description: "The direction of the wind, in degrees. 0: north, 90: east, 180: south, 270: west"
     },
     windGust: {
-      values: "0.0",
+      example: "0.0",
       description: "The maximum wind gust speed, in kilometers per hour."
     },
     windSpeed: {
-      values: "0.0",
+      example: "0.0",
       description: "The wind speed, in kilometers per hour."
     },
     precipitationChance: {
-      values: "0.0",
+      example: "0.0",
       description: "The probability of precipitation during this minute."
     },
     precipitationAmount: {
-      values: "0.0",
+      example: "0.0",
       description: "The amount of precipitation forecasted to occur during period, in millimeters."
     },
     precipitationType: {
-      values: '"rain"',
+      example: '"rain"',
       description: "The type of precipitation forecasted to occur during the period, one of: clear, precipitation, rain, snow, sleet, hail, mixed"
     },
     snowfallIntensity: {
-      values: "0.0",
+      example: "0.0",
       description: "The rate at which snow crystals are falling, in millimeters per hour."
     },
     snowfallAmount: {
-      values: "0.0",
+      example: "0.0",
       description: "The depth of snow as ice crystals forecasted to occur during the period, in millimeters."
     },
     maxUvIndex: {
-      values: "0",
+      example: "0",
       description: "The maximum ultraviolet index value during the day."
     },
     moonPhase: {
-      values: '"firstQuarter"',
+      example: '"firstQuarter"',
       description: "The phase of the moon on the specified day, one of: new, waxingCrescent, firstQuarter, waxingGibbous, full, waningGibbous, thirdQuarter, or waningCrescent."
     },
     moonrise: {
-      values: "2000-01-01T00:00:00Z",
+      example: "2000-01-01T00:00:00Z",
       description: "The time of moonrise on the specified day."
     },
     moonset: {
-      values: "2000-01-01T00:00:00Z",
+      example: "2000-01-01T00:00:00Z",
       description: "The time of moonset on the specified day."
     },
     solarMidnight: {
-      values: "2000-01-01T00:00:00Z",
+      example: "2000-01-01T00:00:00Z",
       description: "The time when the sun is lowest in the sky."
     },
     solarNoon: {
-      values: "2000-01-01T00:00:00Z",
+      example: "2000-01-01T00:00:00Z",
       description: "The time when the sun is highest in the sky."
     },
     sunrise: {
-      values: "2000-01-01T00:00:00Z",
+      example: "2000-01-01T00:00:00Z",
       description: "The time when the top edge of the sun reaches the horizon in the morning."
     },
     sunriseCivil: {
-      values: "2000-01-01T00:00:00Z",
+      example: "2000-01-01T00:00:00Z",
       description: "The time when the sun is 6 degrees below the horizon in the morning."
     },
     sunriseNautical: {
-      values: "2000-01-01T00:00:00Z",
+      example: "2000-01-01T00:00:00Z",
       description: "The time when the sun is 12 degrees below the horizon in the morning."
     },
     sunriseAstronomical: {
-      values: "2000-01-01T00:00:00Z",
+      example: "2000-01-01T00:00:00Z",
       description: "The time when the sun is 18 degrees below the horizon in the morning."
     },
     sunset: {
-      values: "2000-01-01T00:00:00Z",
+      example: "2000-01-01T00:00:00Z",
       description: "The time when the top edge of the sun reaches the horizon in the evening."
     },
     sunsetCivil: {
-      values: "2000-01-01T00:00:00Z",
+      example: "2000-01-01T00:00:00Z",
       description: "The time when the sun is 6 degrees below the horizon in the evening."
     },
     sunsetNautical: {
-      values: "2000-01-01T00:00:00Z",
+      example: "2000-01-01T00:00:00Z",
       description: "The time when the sun is 12 degrees below the horizon in the evening."
     },
     sunsetAstronomical: {
-      values: "2000-01-01T00:00:00Z",
+      example: "2000-01-01T00:00:00Z",
       description: "The time when the sun is 18 degrees below the horizon in the evening."
     },
     temperatureMax: {
-      values: "33.2",
+      example: "33.2",
       description: "The maximum temperature forecasted to occur during the day, in degrees Celsius."
     },
     temperatureMin: {
-      values: "24.8",
+      example: "24.8",
       description: "The minimum temperature forecasted to occur during the day, in degrees Celsius."
     },
     windGustSpeedMax: {
-      values: "0.0",
+      example: "0.0",
       description: "The maximum speed wind is expected to gust to over the period, in kilometers per hour."
     },
     windSpeedAvg: {
-      values: "0.0",
+      example: "0.0",
       description: "The average expected wind speed over the period, in kilometers per hour."
     },
     windSpeedMax: {
-      values: "0.0",
+      example: "0.0",
       description: "The maximum expected wind speed over the period, in kilometers per hour."
     },
   }
