292bebd6c500c55ba18f0789113b443dc8fe5ef0
Loren Norman <lorennorman@gmail.com>
2025-04-07 14:44:24 -0400

    design feedback, prepare for real data

--- app/blocks/power_ups/weather_mixin.js
+++ app/blocks/power_ups/weather_mixin.js
@@ -2,7 +2,7 @@
 // simplifies juggling the weather api keys by period type
 export default {
   // helper to humanize camelCase strings
-  keyToLabel: key => {
+  keyToLabel: function(key) {
     const label = key
       // insert spaces before each capital letter
       .replaceAll(/[A-Z]/g, (match) => `\u00A0${match}`)
@@ -12,6 +12,24 @@ export default {
       .replace(/^[a-z]/, (match) => match.toUpperCase())
 
     return label
+  },
+
+  keyToHelpObject: function(key) {
+    const keyWithoutDayPart = key.split(":").pop()
+
+    return this.HELP_TEXT_BY_PROP[keyWithoutDayPart] || {}
+  },
+
+  keyToTooltip: function(key) {
+    const { description="" } = this.keyToHelpObject(key)
+
+    return `${this.keyToLabel(key)}:\n  ${description}`
+  },
+
+  keyToCurrent: function(key) {
+    const { values="unknown" } = this.keyToHelpObject(key)
+
+    return values
   },
 
   propertyOptionsForTime: function(timeKey) {
@@ -36,8 +54,13 @@ export default {
       throw new Error(`[mixins.weather] timeKey not recognized: ${timeKey}`)
     }
 
+    // TODO: is there a way to add tooltips for each option as well?
     const propertyOptions = optionKeys.reduce((acc, key) => {
-      const label = this.keyToLabel(key)
+      const
+        name = this.keyToLabel(key),
+        current = this.keyToCurrent(key),
+        label = `${name} (Now: ${current})`
+
       acc.push([ label, key ])
 
       return acc
@@ -56,16 +79,15 @@ export default {
     }
 
     const
-      keyWithoutDayPart = propertyKey.split(":").pop(),
-      { values="unknown", description="" } = this.HELP_TEXT_BY_PROP[keyWithoutDayPart],
-      helpText = `${this.keyToLabel(propertyKey)}:\n  ${description}`
+      helpText = this.keyToTooltip(propertyKey),
+      current = this.keyToCurrent(propertyKey)
 
     // set a metric tooltip on dropdown and help text
     propertyField.setTooltip(helpText)
     helpField.setTooltip(helpText)
 
     // update the help text with examples for this metric
-    helpField.setValue(`Ex: ${values}`)
+    helpField.setValue(`Now: ${current}`)
   },
 
   CURRENT_PROPS: [
@@ -169,103 +191,103 @@ export default {
 
   HELP_TEXT_BY_PROP: {
     cloudCover: {
-      values: "0.0 - 1.0",
+      values: "0.0",
       description: "The percentage of the sky covered with clouds during the period."
     },
     cloudCoverLowAltPct: {
-      values: "0.0 - 1.0",
+      values: "0.0",
       description: "The percentage of the sky covered with low-altitude clouds (1800m and below) during the period."
     },
     cloudCoverMidAltPct: {
-      values: "0.0 - 1.0",
+      values: "0.0",
       description: "The percentage of the sky covered with mid-altitude clouds (1800m to 6300m) during the period."
     },
     cloudCoverHighAltPct: {
-      values: "0.0 - 1.0",
+      values: "0.0",
       description: "The percentage of the sky covered with high-altitude clouds (6300m and above) during the period."
     },
     conditionCode: {
-      values: '"Clear", "Cloudy", "Rain"...',
+      values: '"Clear"',
       description: "Weather conditions at the given time, one of: BlowingDust, Clear, MostlyClear, PartlyCloudy, MostlyCloudy, Cloudy, Foggy, Haze, Smoky, Breezy, Windy, Drizzle, Rain, HeavyRain, IsolatedThunderstorms, ScatteredThunderstorms, Thunderstorms, StrongStorms, SunShowers, Frigid, Hail, Hot, Flurries, Sleet, Snow, SunFlurries, WintryMix, Blizzard, BlowingSnow, FreezingDrizzle, FreezingRain, HeavySnow, Hurricane, TroicalStorm"
     },
     daylight: {
-      values: "true, false",
+      values: "true",
       description: "Is there daylight at this time?"
     },
     humidity: {
-      values: "0.0 - 1.0",
+      values: "0.0",
       description: "Relative humidity as a percentage of the total water vapor the air can hold at this temperature."
     },
     precipitationIntensity: {
-      values: "0, 2.5, 13.2, etc",
+      values: "13.2",
       description: "Precipitation intensity measured in millimeters per hour"
     },
     pressure: {
-      values: "1,013.25 +/- a few dozen",
+      values: "1,013.25",
       description: "The sea level air pressure, in millibars."
     },
     pressureTrend: {
-      values: '"rising", "falling", or "steady"',
+      values: '"rising"',
       description: "A string indicating the sea level air pressure trend during this period."
     },
     temperature: {
-      values: "-5.3, 22.7, 38.5...",
+      values: "22.7",
       description: "Temperature in degrees Celsius during this period."
     },
     temperatureApparent: {
-      values: "-5.3, 22.7, 38.5...",
+      values: "38.5...",
       description: "The feels-like temperature when factoring wind and humidity, in degrees Celsius during this period."
     },
     temperatureDewPoint: {
-      values: "-5.3, 22.7, 38.5...",
+      values: "26.3",
       description: "The temperature at which relative humidity is 100%, in degrees Celsius during this period."
     },
     uvIndex: {
-      values: "0 - 11",
+      values: "5",
       description: "The level of ultraviolet radiation during this period."
     },
     visibility: {
-      values: "1000.0, 28520.77, 33085.52...",
+      values: "28520.77",
       description: "The distance at which terrain is visible, in meters. Under 1,000 is considered \"low visibilty\""
     },
     windDirection: {
-      values: "0 - 359",
+      values: "0",
       description: "The direction of the wind, in degrees. 0: north, 90: east, 180: south, 270: west"
     },
     windGust: {
-      values: "0.0 - 407.0",
+      values: "0.0",
       description: "The maximum wind gust speed, in kilometers per hour."
     },
     windSpeed: {
-      values: "0.0 - 407.0",
+      values: "0.0",
       description: "The wind speed, in kilometers per hour."
     },
     precipitationChance: {
-      values: "0.0 - 1.0",
+      values: "0.0",
       description: "The probability of precipitation during this minute."
     },
     precipitationAmount: {
-      values: "0.0, 2.3, 7.8...",
+      values: "0.0",
       description: "The amount of precipitation forecasted to occur during period, in millimeters."
     },
     precipitationType: {
-      values: '"clear", "rain", "snow"...',
+      values: '"rain"',
       description: "The type of precipitation forecasted to occur during the period, one of: clear, precipitation, rain, snow, sleet, hail, mixed"
     },
     snowfallIntensity: {
-      values: "0.0, 2.7, 8.3...",
+      values: "0.0",
       description: "The rate at which snow crystals are falling, in millimeters per hour."
     },
     snowfallAmount: {
-      values: "0.0, 1.4, 3.8...",
+      values: "0.0",
       description: "The depth of snow as ice crystals forecasted to occur during the period, in millimeters."
     },
     maxUvIndex: {
-      values: "0 - 11",
+      values: "0",
       description: "The maximum ultraviolet index value during the day."
     },
     moonPhase: {
-      values: '"new", "firstQuarter", "waningCrescent"...',
+      values: '"firstQuarter"',
       description: "The phase of the moon on the specified day, one of: new, waxingCrescent, firstQuarter, waxingGibbous, full, waningGibbous, thirdQuarter, or waningCrescent."
     },
     moonrise: {
@@ -317,23 +339,23 @@ export default {
       description: "The time when the sun is 18 degrees below the horizon in the evening."
     },
     temperatureMax: {
-      values: "-3.5, 12.3, 35.0",
+      values: "33.2",
       description: "The maximum temperature forecasted to occur during the day, in degrees Celsius."
     },
     temperatureMin: {
-      values: "-3.5, 12.3, 35.0",
+      values: "24.8",
       description: "The minimum temperature forecasted to occur during the day, in degrees Celsius."
     },
     windGustSpeedMax: {
-      values: "0.0 - 407.0",
+      values: "0.0",
       description: "The maximum speed wind is expected to gust to over the period, in kilometers per hour."
     },
     windSpeedAvg: {
-      values: "0.0 - 407.0",
+      values: "0.0",
       description: "The average expected wind speed over the period, in kilometers per hour."
     },
     windSpeedMax: {
-      values: "0.0 - 407.0",
+      values: "0.0",
       description: "The maximum expected wind speed over the period, in kilometers per hour."
     },
   }
