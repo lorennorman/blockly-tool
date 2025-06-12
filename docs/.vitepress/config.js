4b177b118ee42114e1fb15771b2c9d741285db89
Loren Norman <lorennorman@gmail.com>
2025-06-12 14:00:10 -0400

    dynamic sidebar generation

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitepress'
+import blocksSidebar from '../blocks/_blocks_sidebar.json'
 
 
 const REPO = 'https://github.com/lorennorman/blockly-tool'
@@ -8,10 +9,19 @@ export default defineConfig({
   title: "IO Actions: Block Reference",
   description: "Documentation for Adafruit IO's block-based Actions",
 
+  head: [
+    ['link', { rel: 'icon', href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧩</text></svg>" }]
+  ],
+
   // https://vitepress.dev/reference/default-theme-config
   themeConfig: {
+    search: {
+      provider: 'local'
+    },
+
     editLink: {
       pattern: ({ filePath }) => {
+        // runs on the frontend, must be a pure function
         const jsPath = filePath.replace(/.md$/, '.js')
 
         return `https://github.com/lorennorman/blockly-tool/edit/main/app/${jsPath}`
@@ -24,13 +34,7 @@ export default defineConfig({
     ],
 
     sidebar: [
-      {
-        text: 'Blocks',
-        items: [
-          { text: 'Email', link: '/blocks/action/email' },
-          { text: 'Log', link: '/blocks/action/log' }
-        ]
-      }
+      blocksSidebar
     ],
 
     socialLinks: [
--- export_documentation.js
+++ export_documentation.js
@@ -21,7 +21,6 @@ const withCleanDir = async (dirName, writeFunction) => {
 
     // ensure dir is present before writing
     if(!fs.existsSync(exportDirname)) {
-      console.log("mkdir:", exportDirname)
       fs.mkdirSync(exportDirname, { recursive: true })  
     }
 
@@ -44,36 +43,68 @@ import { importBlockJson, importBlockDefinitions, allBlockDefinitionsAndPaths } 
 import importToolboxJson from './src/importer/toolbox_importer.js'
 import importWorkspaceJson from './src/importer/workspace_importer.js'
 import importBlocklyJs from './src/importer/blockly_importer.js'
-import { map } from 'lodash-es'
+import { capitalize, filter, find, forEach, get, keyBy, map, mapValues, sortBy } from 'lodash-es'
 
+const
+  toolbox = await importToolboxJson(),
+  categories = filter(toolbox.contents, { kind: 'category' }),
+  categoryBlocksMap = mapValues(keyBy(categories, "name"), cat => map(cat.contents, "type"))
 
+const pretty = jsObject => JSON.stringify(jsObject, null, 2) + "\n"
 
 const toBlockMarkdown = definition => {
   return `---
-title: "Block: ${definition.type}"
+title: "Block: ${definition.name}"
 editLink: true
 ---
-${ definition.visualization?.tooltip || "No docs for this block, yet." }
+
+# Block: ${definition.name}
+
+    ${ definition.visualization?.tooltip?.replaceAll("\n", "\n    ") || "No docs for this block, yet." }
 `
 }
 
 withCleanDir("docs/blocks", async write => {
-  map(allBlockDefinitionsAndPaths, ({ path, definition }) => {
+  const blockSidebar = {
+    text: 'Blocks',
+    items: map(categoryBlocksMap, (blocks, categoryName) => {
+      return {
+        text: categoryName,
+        collapsed: true,
+        items: []
+      }
+    })
+  }
+
+  forEach(allBlockDefinitionsAndPaths, ({ path, definition }) => {
     // skip disabled blocks
     if(definition.disabled) { return }
 
-    const blockKey = definition.type
+    // set a default name if missing
+    if(!definition.name) {
+      definition.name = capitalize(definition.type.replaceAll("_", " ").replace(/^io /, ""))
+    }
 
     // mirror the blocks/**/*.js path structure
     const docPath = path.replace(/.js$/, '.md')
-    // console.log(blockKey, docPath)
     write(docPath, toBlockMarkdown(definition))
-    // console.log(key, Object.keys(definition))
-  })
-  // write("blocks.json", pretty(await importBlockJson()))
-  // write("toolbox.json", pretty(await importToolboxJson()))
-  // write("workspace.json", pretty(await importWorkspaceJson()))
 
-  // // JS
-  // write("blockly.js", await importBlocklyJs())
+    const
+      blockSidebarPath = `/blocks/${docPath.slice(0, -3)}`,
+      sidebarEntry = {
+        text: capitalize(definition.name),
+        link: blockSidebarPath
+      }
+
+    // add block links to the appropriate sidebar
+    forEach(categoryBlocksMap, (categoryBlocks, categoryName) => {
+      // if category contains this block, add to its sidebar
+      if(categoryBlocks.includes(definition.type)) {
+        console.log('found:', definition.type, 'in', categoryName)
+        find(blockSidebar.items, { text: categoryName }).items.push(sidebarEntry)
+      }
+    })
+  })
+
+  write('_blocks_sidebar.json', pretty(blockSidebar))
 })
