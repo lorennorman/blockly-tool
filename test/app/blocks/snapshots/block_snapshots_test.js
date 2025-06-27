6dc042d0b31bf10eab586b9a2fd11e0f1a20aeb7
Loren Norman <lorennorman@gmail.com>
2025-06-27 16:22:26 -0400

    snapshot all blocks json

---
+++ test/app/blocks/snapshots/block_snapshots_test.js
@@ -0,0 +1,15 @@
+import { describe, it } from 'node:test'
+
+import BlockDefinition from "#src/block_definition.js"
+
+
+describe("Block Snapshots", { only: true }, async () => {
+  // load all current blocks
+  (await BlockDefinition.loadAll()).forEach(blockDefinition => {
+    // define a test for each block
+    it(`"${blockDefinition.name}" (${blockDefinition.type})`, ({ assert: { snapshot }}) => {
+      // test merely checks the json representation
+      snapshot(blockDefinition.toBlocklyJSON())
+    })
+  })
+})
