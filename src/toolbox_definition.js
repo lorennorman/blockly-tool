ac6f8eab1afe12962a77144773bfcffc45ae9668
Loren Norman <lorennorman@gmail.com>
2025-07-11 18:19:25 -0400

    test and move toolbox exports

--- src/toolbox_definition.js
+++ src/toolbox_definition.js
@@ -1,6 +1,4 @@
-import { forEach, filter } from 'lodash-es'
-
-import { exportToolboxJSON } from '#src/importer/toolbox_importer.js'
+import { forEach, filter, map } from 'lodash-es'
 
 
 class ToolboxDefinition {
@@ -13,8 +11,12 @@ class ToolboxDefinition {
   // just the categories
   categories = []
 
-  toBlocklyJSONString = async function() {
-    return JSON.stringify(await exportToolboxJSON(this), null, 2) + "\n"
+  toBlocklyJSON = function() {
+    return ToolboxExporter.exportBlocklyJSON(this)
+  }
+
+  toBlocklyJSONString = function() {
+    return JSON.stringify(this.toBlocklyJSON(), null, 2) + "\n"
   }
 }
 
@@ -26,6 +28,8 @@ ToolboxDefinition.parseRawDefinition = function(definition, definitionSet) {
   toolboxDefinition.definitionJS = definition
 
   forEach(definition, item => {
+    // copy the item before we start modifying it
+    item = { ...item }
     // all items go into contents
     toolboxDefinition.contents.push(item)
 
@@ -46,4 +50,78 @@ ToolboxDefinition.parseRawDefinition = function(definition, definitionSet) {
   })
 
   return toolboxDefinition
+}
+
+
+const ToolboxExporter = {
+  exportBlocklyJSON: toolboxDefinition => {
+    const contents = ToolboxExporter.generateToolboxContents(toolboxDefinition.contents)
+
+    return {
+      kind: 'categoryToolbox',
+      contents
+    }
+  },
+
+  generateToolboxContents: rawContents => map(rawContents, category => {
+    if(category.name) {
+      return ToolboxExporter.generateCategoryFromDefinition(category)
+    }
+
+    return category
+  }),
+
+  generateCategoryFromDefinition: category => {
+    // log(`- "${category.name}"`)
+
+    // validateCategoryDefinition(category)
+
+    const
+      contents = ToolboxExporter.generateCategoryContents(category),
+      contentKinds = map(contents, "kind"),
+      blockCount = filter(contentKinds, kind => kind == "block").length,
+      labelCount = filter(contentKinds, kind => kind == "label").length
+
+    // log(`  - ${blockCount} blocks added`)
+    if(labelCount) {
+      // log(`  - ${labelCount} labels added`)
+    }
+
+    if(!contents.length && !category.callback) {
+      // warn(`  - Warning: no blocks generated for category without callback "${category.name}"!`)
+    }
+
+    // inject other kinds of toolbox objects here
+    const categoryItem = {
+      kind: 'category',
+      name: category.name,
+      colour: (category.colour === 0) ? "0" : category.colour,
+      contents
+    }
+
+    if(category.callback) {
+      categoryItem.custom = category.name
+    }
+
+    return categoryItem
+  },
+
+  generateCategoryContents: ({ name, contents, callback }) => {
+    // no contents?
+    if (!contents) {
+      // better have a callback
+      if(callback) { return [] }
+      // otherwise we have a problem
+      throw new Error(`No contents found for category "${name}"`)
+    }
+
+    const toolboxContents = map(contents, blockDefinition => ({
+      kind: 'block',
+      ...blockDefinition.toBlocklyInstanceJSON()
+    }))
+
+    // warnOnExtraBlocksSpecifyCategory(name, definitionContents)
+
+    return toolboxContents
+  },
 }
