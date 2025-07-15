3c9df96d015f9ffa929fa168fddd0f82e95c3f4a
Loren Norman <lorennorman@gmail.com>
2025-07-15 14:12:20 -0400

    disable vite plugin, delete more dead files

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
-import { defineConfig, loadEnv } from 'vite'
-
-import ImportUserAppPlugin from './src/importer/vite_plugin.js'
+import { defineConfig } from 'vite'
 
 
-export default defineConfig(({ mode }) => {
-  const
-    env = loadEnv(mode, process.cwd(), ''),
-    plugins = env.NODE_ENV == 'development'
-      ? [ ImportUserAppPlugin() ]
-      : []
-
-  return {
-    plugins
-  }
+export default defineConfig(() => {
+  return {}
 })
